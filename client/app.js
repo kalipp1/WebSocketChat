8873ebb263a0c9c3bacc043e54014f44aedce424
Maks Birecki <maksymilian.birecki@gmail.com>
2024-12-20 16:47:49 +0100

    add Chat functionalities

--- client/app.js
+++ client/app.js
@@ -4,10 +4,19 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+const socket = io();
 
 console.log(loginForm);
 
 let userName = '';
+
+socket.on('message', ({ author, content }) => addMessage(author, content))
+socket.on('join' , ({ author }) => {
+    addSystemMessage('ChatBot', `${author} has joined the chat`);
+})
+socket.on('disconnectUser' , ({ author }) => {
+    addSystemMessage('ChatBot', `${author} has left the chat`);
+})
 
 const login = (e) => {
     e.preventDefault();
@@ -18,6 +27,7 @@ const login = (e) => {
         userName = userNameInput.value;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('login', userName);
     }
 }
 
@@ -39,19 +49,39 @@ const addMessage = (author, content) => {
     messagesList.appendChild(message);
 }
 
-const sendMessage = (e) => {
-    e.preventDefault();
-    if(!messageContentInput.value){
-        alert('You need to type in message before sending');
-        return;
-    }else{
-        addMessage(userName, messageContentInput.value);
-        messageContentInput.value = "";
-    }
+const addSystemMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--system');
+    message.innerHTML = `
+        <h3 class="message__author">
+            ${author === userName ? 'You' : author}
+        </h3>
+        <div class="message__content">
+            ${content}
+        </div>
+    `;
+    messagesList.appendChild(message);
 }
 
+function sendMessage(e) {
+    e.preventDefault();
+  
+    let messageContent = messageContentInput.value;
+  
+    if(!messageContent.length) {
+      alert('You have to type something!');
+    }
+    else {
+      addMessage(userName, messageContent);
+      socket.emit('message', { author: userName, content: messageContent })
+      messageContentInput.value = '';
+    }
+  
+  }
+
 loginForm.addEventListener('submit', login);
-addMessageForm.addEventListener('submit', sendMessage)
+addMessageForm.addEventListener('submit', sendMessage);
 
 
 
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
 
 const messages = [];
+const users = [];
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
 
-app.listen(8000, () => {
+const server = http.createServer(app);
+
+server.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('login', (userName) => {
+    users.push({ name: userName, id: socket.id });
+    console.log(`User logged in: ${userName}, ID: ${socket.id}`);
+    console.log('Current users:', users);
+
+    socket.broadcast.emit('join', { author: userName });
+  });
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => { 
+    console.log('Oh, socket ' + socket.id + ' has left') 
+    const index = users.findIndex((user) => user.id === socket.id)
+    if(index !== -1) {
+      const userName = users[index].name;
+      console.log(`User disconnected: ${userName}, ID: ${socket.id}`);
+      socket.broadcast.emit('disconnectUser', { author: userName});
+
+      users.splice(index, 1);
+
+      console.log('Current users:', users);
+    }
+  });
+  console.log('I\'ve added a listener on message and disconnect events \n');
 });
